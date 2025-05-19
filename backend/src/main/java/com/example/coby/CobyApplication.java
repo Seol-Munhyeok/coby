d73bcf052162f04efab384c9b581840e6127518c
Seol-Munhyeok <abc1234568@naver.com>
2025-05-19 13:08:01 +0900

    Initial commit with backend, frontend, judge

---
+++ backend/src/main/java/com/example/coby/CobyApplication.java
@@ -0,0 +1,13 @@
+package com.example.coby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CobyApplication.class, args);
+	}
+
+}
