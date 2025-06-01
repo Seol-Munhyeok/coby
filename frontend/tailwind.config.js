91e319a939a1e9d7750a6b30a125cac4f25368b5
Lee Gyeongmin <lkm1282@naver.com>
2025-06-01 17:52:39 +0900

    메인페이지 리액트 구현

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+     "./src/**/*.{js,jsx,ts,tsx}",  // JSX 파일 포함
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
