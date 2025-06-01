ac9bc8662f233f962a833b19aa18581391b5f12c
Lee Gyeongmin <lkm1282@naver.com>
2025-06-01 17:52:39 +0900

    메인페이지 리액트 구현

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -3,6 +3,10 @@ import { Routes, Route } from "react-router-dom";
 // import Main from "./pages/Main/Main";
 import Login from "./pages/Login/Login";
 import NicknamePopup from "./pages/NickName/NicknamePopup";
+import MainPage from "./pages/Main/MainPage";
+import WaitingRoom from "./pages/WaitingRoom/WaitingRoom";
+import MyPage from "./pages/MyPage/MyPage";
+
 
 const Router = () => {
 
@@ -11,7 +15,11 @@ const Router = () => {
       <Route path="/" element={<Login />} />
       {/* <Route path="/URL상 경로" element={<TEST />} /> */}
       <Route path="/nickname" element={<NicknamePopup />}/>
-    
+      <Route path="/mainpage" element={<MainPage />}/>
+      <Route path="/waitingRoom" element={<WaitingRoom />}/>      
+      <Route path="/myPage" element={<MyPage />}/>      
+
+
     </Routes>
   );
 };
