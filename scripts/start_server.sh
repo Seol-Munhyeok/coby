8acc97ac2de89c096f5908a494434811124016cf
김정우 <51903566+custiya@users.noreply.github.com>
2025-08-29 20:00:48 +0900

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,6 +4,7 @@ APP_DIR="/home/ubuntu/app"
 JAR_FILE=$(find $APP_DIR/backend -name "*.jar" | head -n 1)
 LOG_FILE="/home/ubuntu/app/application.log"
 
+source $APP_DIR/scripts/env.sh
 sudo cp -r $APP_DIR/frontend/build/* /var/www/html
 
 nohup java -jar $JAR_FILE > $LOG_FILE 2>&1 &
