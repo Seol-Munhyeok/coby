c6b865ba5d21b905fe02f6f75fe62f01d3815752
정우 김 <kjwjerry@naver.com>
2025-09-09 15:22:04 +0900

    fix conflict

--- backend/src/main/java/com/example/coby/dto/SubmissionResponseDto.java
+++ backend/src/main/java/com/example/coby/dto/SubmissionResponseDto.java
@@ -13,7 +13,13 @@ public class SubmissionResponseDto {
     @JsonProperty("submission_id")
     private Long submissionId;
 
-    @JsonProperty("result")
-    private String status;
+    // 변수 이름을 'status'에서 'result'로 변경하여 JSON 키와 일치시킵니다.
+    private String result;
+
     private String details;
+    @JsonProperty("avg_time")
+    private Double avgTime;
+
+    @JsonProperty("avg_memory")
+    private Double avgMemory;
 }
