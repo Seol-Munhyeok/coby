5cb7b5a0b3e09e6641b7154adaf8cf206ecc863b
Lee Gyeongmin <lkm1282@naver.com>
2025-09-30 00:40:25 +0900

    방이 삭제될때, 그 방에 속한 모든 RoomUser을 제거하는 코드 추가

--- backend/src/main/java/com/example/coby/repository/RoomUserRepository.java
+++ backend/src/main/java/com/example/coby/repository/RoomUserRepository.java
@@ -3,6 +3,7 @@ package com.example.coby.repository;
 import com.example.coby.entity.RoomUser;
 import com.example.coby.entity.RoomUserId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -10,4 +11,7 @@ public interface RoomUserRepository extends JpaRepository<RoomUser, RoomUserId> 
     List<RoomUser> findByRoomId(Long roomId);
     long countByRoomId(Long roomId);
 
+    // 이 메소드는 roomId에 해당하는 RoomUser를 모두 삭제합니다.
+    @Transactional
+    void deleteAllByRoomId(Long roomId);
 }
